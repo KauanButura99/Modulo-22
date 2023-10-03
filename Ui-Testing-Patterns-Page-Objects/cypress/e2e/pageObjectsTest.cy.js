99d5140bb4d10afb77050235477ef9a48c60ea04
Kauan Butura <marciobutura@outlook.com>
2023-10-02 22:24:33 -0300

    Primeiro commit

---
+++ Ui-Testing-Patterns-Page-Objects/cypress/e2e/pageObjectsTest.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+const { dashbordPage, LoginPage } = require("../support/pages/index")
+const data = require("../fixtures/data.json");
+//const LoginPage = require("../support/pages/login.page")
+//const { dashbordPage } = require("../support/pages/dashboard.page");
+
+describe('Access Admin Painel', () => {
+  beforeEach(() => {
+    cy.visit("/wp-admin")
+  });
+
+  it('I should login with valid credentials', () => {
+    LoginPage.login(data.usuario, data.senha)
+    dashbordPage.siteName.should("be.visible")
+  });
+});
---
+++ Ui-Testing-Patterns-Page-Objects/cypress/support/pages/index.js
@@ -0,0 +1,4 @@
+import LoginPage from "./login.page";
+import { dashbordPage } from "./dashboard.page";
+
+export { LoginPage, dashbordPage }
