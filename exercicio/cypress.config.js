d100f34825eef713bcab267e4432a5886983b7f2
Kauan <marciobutura@outlook.com>
2024-03-06 13:08:14 -0300

    mochawesome report

--- exercicio/cypress.config.js
+++ exercicio/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+const Mochawesome = require("mochawesome");
 
 async function setupNodeEvents(on, config) {
   await preprocessor.addCucumberPreprocessorPlugin(on, config);
@@ -15,7 +16,12 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
     specPattern: "cypress/e2e/*.feature",
-    setupNodeEvents
+    setupNodeEvents,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportFilename: "[name]-result",
+      html: true
+    }
   },
 });
 
